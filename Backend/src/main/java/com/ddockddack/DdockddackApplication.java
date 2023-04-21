e7a4c8177c6fe3cbd01672f7735a74265593fb75
Taekyun Kim <taekyun.contact@gmail.com>
2023-04-21 13:41:31 +0900

    feat : @EnableScheduling 추가

--- Backend/src/main/java/com/ddockddack/DdockddackApplication.java
+++ Backend/src/main/java/com/ddockddack/DdockddackApplication.java
@@ -7,7 +7,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.repository.config.BootstrapMode;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableAspectJAutoProxy
