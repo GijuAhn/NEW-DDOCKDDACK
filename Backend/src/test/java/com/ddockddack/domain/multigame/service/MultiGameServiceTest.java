f668428040ea75a0c8d944ecc2330089cca93403
Taekyun Kim <taekyun.contact@gmail.com>
2023-05-01 10:26:14 +0900

    test : Test template 작성

---
+++ Backend/src/test/java/com/ddockddack/domain/multigame/service/MultiGameServiceTest.java
@@ -0,0 +1,52 @@
+package com.ddockddack.domain.multigame.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultiGameServiceTest {
+
+    @Test
+    void findAllGames() {
+    }
+
+    @Test
+    void findGame() {
+    }
+
+    @Test
+    void saveGame() {
+    }
+
+    @Test
+    void modifyGame() {
+    }
+
+    @Test
+    void removeGame() {
+    }
+
+    @Test
+    void starredGame() {
+    }
+
+    @Test
+    void unStarredGame() {
+    }
+
+    @Test
+    void reportGame() {
+    }
+
+    @Test
+    void findAllGamesByMemberId() {
+    }
+
+    @Test
+    void findAllStarredGames() {
+    }
+
+    @Test
+    void findAllReportedGames() {
+    }
+}
