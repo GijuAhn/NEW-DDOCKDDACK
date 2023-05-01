8eaf1c04e373b342b49efd75fbffc38252040e88
Taekyun Kim <taekyun.contact@gmail.com>
2023-05-01 12:45:14 +0900

    test : findAllStarredGames 테스트 메서드 추가 및 makeStarredGameRes 메서드 추가

--- Backend/src/test/java/com/ddockddack/domain/multigame/service/MultiGameServiceTest.java
+++ Backend/src/test/java/com/ddockddack/domain/multigame/service/MultiGameServiceTest.java
@@ -1,6 +1,11 @@
 package com.ddockddack.domain.multigame.service;
 
+import com.ddockddack.domain.member.entity.Member;
+import com.ddockddack.domain.member.entity.Role;
+import com.ddockddack.domain.member.repository.MemberRepository;
+import com.ddockddack.domain.multigame.repository.StarredGameRepository;
 import com.ddockddack.domain.multigame.response.ReportedGameRes;
+import com.ddockddack.domain.multigame.response.StarredGameRes;
 import com.ddockddack.domain.report.repository.ReportedGameRepository;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,8 +16,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -20,20 +27,35 @@ import static org.mockito.Mockito.*;
 @TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
 @ExtendWith(MockitoExtension.class)
 class MultiGameServiceTest {
+    private final Long memberOneId = 1L;
 
     @InjectMocks
     MultiGameService multiGameService;
-
     @Mock
     ReportedGameRepository reportedGameRepository;
+    @Mock
+    StarredGameRepository starredGameRepository;
+    @Mock
+    MemberRepository memberRepository;
 
     Logger log;
+
+    private Member memberOne;
 
 
     @Transactional
     @BeforeEach
     public void setUp() {
         log = (Logger) LoggerFactory.getLogger(ReportedGameRepository.class);
+
+        memberOne = Member.builder()
+            .email("test@test.com")
+            .nickname("테스터닉네임")
+            .profile("testerProfile.jpg")
+            .role(Role.MEMBER)
+            .build();
+        memberOne.setId(memberOneId);
+        memberOne.setReleaseDate(LocalDate.now().plusDays(30));
     }
 
 
@@ -74,8 +96,29 @@ class MultiGameServiceTest {
     }
 
     @Test
+    @DisplayName("즐겨찾기한 게임 리스트 조회")
+    @Order(2)
     void findAllStarredGames() {
+        // given
+        List<StarredGameRes> allStarredGames = new ArrayList<>();
+        StarredGameRes starredGameRes = makeStarredGameRes();
+        allStarredGames.add(starredGameRes);
+        when(memberRepository.findById(memberOneId)).thenReturn(Optional.of(memberOne));
+        doReturn(allStarredGames).when(starredGameRepository).findAllStarredGame(memberOneId);
+
+        // when
+        List<StarredGameRes> result  = multiGameService.findAllStarredGames(memberOneId);
+
+        // then
+        assertThat(result.size()).isEqualTo(1);
+        assertThat(result.get(0).getGameId()).isEqualTo(1L);
+        assertThat(result.get(0).getGameTitle()).isEqualTo("테스트 게임 타이틀");
+
+        // verify
+        verify(memberRepository, times(1)).findById(memberOneId);
+        verify(starredGameRepository, times(1)).findAllStarredGame(memberOneId);
     }
+
 
     @Test
     @DisplayName("신고된 게임 리스트 조회")
@@ -111,5 +154,19 @@ class MultiGameServiceTest {
         reportedGameRes.setReportedMemberNickname("테스트 피신고자 닉네임");
 
         return reportedGameRes;
+    }
+
+    private StarredGameRes makeStarredGameRes() {
+        StarredGameRes starredGameRes = new StarredGameRes();
+        starredGameRes.setGameId(1L);
+        starredGameRes.setGameTitle("테스트 게임 타이틀");
+        starredGameRes.setGameDesc("테스트 게임 상세 설명");
+        starredGameRes.setCreator("creator");
+        starredGameRes.setIsStarred(1);
+        starredGameRes.setStarredCnt(10);
+        starredGameRes.setPopularity(100);
+        starredGameRes.setThumbnail("테스트 썸네일.jpeg");
+
+        return starredGameRes;
     }
 }
