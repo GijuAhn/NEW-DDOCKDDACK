cb495dace3b90b359995875f0ce36d46b7404038
Taekyun Kim <taekyun.contact@gmail.com>
2023-05-01 11:05:16 +0900

    test : findAllReportedGames 테스트 메서드 추가 및 makeReportedGameRes 메서드 추가

--- Backend/src/test/java/com/ddockddack/domain/multigame/service/MultiGameServiceTest.java
+++ Backend/src/test/java/com/ddockddack/domain/multigame/service/MultiGameServiceTest.java
@@ -1,10 +1,41 @@
 package com.ddockddack.domain.multigame.service;
 
-import org.junit.jupiter.api.Test;
+import com.ddockddack.domain.multigame.response.ReportedGameRes;
+import com.ddockddack.domain.report.repository.ReportedGameRepository;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
+@ExtendWith(MockitoExtension.class)
 class MultiGameServiceTest {
+
+    @InjectMocks
+    MultiGameService multiGameService;
+
+    @Mock
+    ReportedGameRepository reportedGameRepository;
+
+    Logger log;
+
+
+    @Transactional
+    @BeforeEach
+    public void setUp() {
+        log = (Logger) LoggerFactory.getLogger(ReportedGameRepository.class);
+    }
+
 
     @Test
     void findAllGames() {
@@ -47,6 +78,38 @@ class MultiGameServiceTest {
     }
 
     @Test
+    @DisplayName("신고된 게임 리스트 조회")
+    @Order(1)
     void findAllReportedGames() {
+        // given
+        List<ReportedGameRes> allReportedGames = new ArrayList<>();
+        ReportedGameRes reportedGameRes = makeReportedGameRes();
+        allReportedGames.add(reportedGameRes);
+        doReturn(allReportedGames).when(reportedGameRepository).findAllReportedGame();
+
+        // when
+        List<ReportedGameRes> result  = multiGameService.findAllReportedGames();
+
+        // then
+        assertThat(result.size()).isEqualTo(1);
+        assertThat(result.get(0).getReportId()).isEqualTo(1L);
+        assertThat(result.get(0).getGameTitle()).isEqualTo("테스트 게임 타이틀");
+
+        // verify
+        verify(reportedGameRepository, times(1)).findAllReportedGame();
+    }
+
+    private ReportedGameRes makeReportedGameRes() {
+        ReportedGameRes reportedGameRes = new ReportedGameRes();
+        reportedGameRes.setReportId(1L);
+        reportedGameRes.setReportMemberId(2L);
+        reportedGameRes.setReportedMemberId(3L);
+        reportedGameRes.setGameId(4L);
+        reportedGameRes.setReason("테스트 사유입니다.");
+        reportedGameRes.setGameTitle("테스트 게임 타이틀");
+        reportedGameRes.setReportMemberNickname("테스트 신고자 닉네임");
+        reportedGameRes.setReportedMemberNickname("테스트 피신고자 닉네임");
+
+        return reportedGameRes;
     }
 }
