26d44080643619827c172df924dfde19a166422d
오세솔 <tpthf1@gmail.com>
2023-05-03 09:26:08 +0900

    feat : GA 적용

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vueClickOutsideElement from "vue-click-outside-element";
+import VueGtag from "vue-gtag-next";
 
-createApp(App).use(store).use(router).use(vueClickOutsideElement).mount("#app");
+const app = createApp(App);
+
+app.use(VueGtag, {
+  property: { id: "G-ZJZ24GL16W" },
+});
+
+app.use(store).use(router).use(vueClickOutsideElement).mount("#app");
