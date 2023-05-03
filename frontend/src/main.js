c63c205aee6a78e3d7eb6eb5744584e8e2c058d9
오세솔 <tpthf1@gmail.com>
2023-05-03 09:54:55 +0900

    feat : GA router 설정

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,8 +7,12 @@ import VueGtag from "vue-gtag-next";
 
 const app = createApp(App);
 
-app.use(VueGtag, {
-  property: { id: "G-ZJZ24GL16W" },
-});
+app.use(
+  VueGtag,
+  {
+    property: { id: "G-ZJZ24GL16W", params: { send_page_view: false } },
+  },
+  router
+);
 
 app.use(store).use(router).use(vueClickOutsideElement).mount("#app");
