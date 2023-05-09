7542a7725bff6d16b741b4995a29bc98635f08ff
오세솔 <tpthf1@gmail.com>
2023-05-09 17:44:45 +0900

    fix : mobile에서 싱글 게임 안되는 문제 수정

--- frontend/src/components/GameList/MobileSingleGameList.vue
+++ frontend/src/components/GameList/MobileSingleGameList.vue
@@ -140,6 +140,7 @@ const callApi = () => {
 const analysis = async () => {
   let fd = new FormData();
   fd.append("target", games.value[targetGameIdx.value].thumbnail);
+  fd.append("singleGameId", games.value[targetGameIdx.value].id);
   let file = await getFile(mode.value);
 
   fd.append("source", file);
